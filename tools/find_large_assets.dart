37780130d87ddb534883e314464f51590c4f38c9
Eddie Zhang <jjzhang2000@hotmail.com>
2026-03-02 10:28:23 +0800

    fix: 更新 tools 目录以兼容 Dart 3.0 和 image 包 4.x

--- tools/find_large_assets.dart
+++ tools/find_large_assets.dart
@@ -2,23 +2,20 @@ import 'dart:io';
 import 'package:image/image.dart';
 
 // This tool helps locate large images.
-void main() 
-{
-	new Directory("../app/assets").list(recursive: true, followLinks: false)
-    .listen((FileSystemEntity entity) 
-	{
-		int dot = entity.path.lastIndexOf(".");
-		if(dot != -1)
-		{
-			String extension = entity.path.substring(dot+1).toLowerCase();
-			if(extension == "png")
-			{
-				Image image = decodeImage(new File(entity.path).readAsBytesSync());
-				if(image.width > 512 && image.height > 512)
-				{
-					print("Large image: ${image.width}x${image.height} - ${entity.path}");
-				}
-			}
-		}
-    });
+void main() {
+  Directory('../app/assets').list(recursive: true, followLinks: false).listen(
+    (FileSystemEntity entity) {
+      int dot = entity.path.lastIndexOf('.');
+      if (dot != -1) {
+        String extension = entity.path.substring(dot + 1).toLowerCase();
+        if (extension == 'png') {
+          final bytes = File(entity.path).readAsBytesSync();
+          final image = decodeImage(bytes);
+          if (image != null && image.width > 512 && image.height > 512) {
+            print('Large image: ${image.width}x${image.height} - ${entity.path}');
+          }
+        }
+      }
+    },
+  );
 }
--- tools/resize_large_assets.dart
+++ tools/resize_large_assets.dart
@@ -2,33 +2,34 @@ import 'dart:io';
 import 'package:image/image.dart';
 
 // This tool resizes large images.
-const String source = "../app/full_quality";
-const String dest = "../app/assets";
+const String source = '../app/full_quality';
+const String dest = '../app/assets';
 
-const int ThresholdSize = 1024;
+const int thresholdSize = 1024;
 
-void main() 
-{
-	new Directory("../app/full_quality").list(recursive: true, followLinks: false)
-    .listen((FileSystemEntity entity) 
-	{
-		int dot = entity.path.lastIndexOf(".");
-		if(dot != -1)
-		{
-			String extension = entity.path.substring(dot+1).toLowerCase();
-			if(extension == "png")
-			{
-				Image image = decodeImage(new File(entity.path).readAsBytesSync());
-				if(image.width > ThresholdSize || image.height > ThresholdSize)
-				{
-					print("Large image: ${image.width}x${image.height} - ${entity.path}");
-					String destFilename = dest + entity.path.substring(source.length);
-					
-					Image thumbnail = image.width > ThresholdSize ? copyResize(image, ThresholdSize, -1) : copyResize(image, -1, ThresholdSize);
-					new File(destFilename)..writeAsBytesSync(encodePng(thumbnail));
-					print("Wrote to: $destFilename");
-				}
-			}
-		}
-    });
+void main() {
+  Directory('../app/full_quality').list(recursive: true, followLinks: false).listen(
+    (FileSystemEntity entity) {
+      int dot = entity.path.lastIndexOf('.');
+      if (dot != -1) {
+        String extension = entity.path.substring(dot + 1).toLowerCase();
+        if (extension == 'png') {
+          final bytes = File(entity.path).readAsBytesSync();
+          final image = decodeImage(bytes);
+          if (image != null) {
+            if (image.width > thresholdSize || image.height > thresholdSize) {
+              print('Large image: ${image.width}x${image.height} - ${entity.path}');
+              String destFilename = dest + entity.path.substring(source.length);
+
+              final thumbnail = image.width > thresholdSize
+                  ? copyResize(image, width: thresholdSize)
+                  : copyResize(image, height: thresholdSize);
+              File(destFilename).writeAsBytesSync(encodePng(thumbnail));
+              print('Wrote to: $destFilename');
+            }
+          }
+        }
+      }
+    },
+  );
 }
